6c11858c73f9f68ace941e7a412d7b8cd8bbc828
Melanie Phan <melanie.phan@prudential.com>
2019-11-15 15:47:02 -0500

    created react skeleton and 3 components for homepage

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
 import React from 'react';
 import './App.css';
+import Nav from './components/Nav'
+import Gallery from './components/Gallery'
+import HomePageBottom from './components/HomePageBottom'
 
 function App() {
   return (
     <div className="App">
-
+      <Nav />
+      <Gallery />
+      <HomePageBottom />
     </div>
   );
 }
---
+++ src/components/HomePageBottom.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function HomePageBottom() {
+  return (
+    <div>
+            <h1>This is the BottomPage</h1>
+
+    </div>
+  )
+}
