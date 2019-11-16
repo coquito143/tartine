4e50f4c998a78a1f6a8e619841deb1a5011830d5
Sujith  John <sujithjohn@Sujiths-MacBook-Pro.local>
2019-11-16 13:03:42 -0500

    navfirstsave

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -2,8 +2,12 @@ import React from 'react'
 
 export default function Nav() {
   return (
-    <div>
-      <h1>This is the Nav</h1>
-    </div>
+    <ul class="topnav">
+      <li><a href="#gallery"> Gallery</a></li>
+      <li><a href="#bakery">Bakery</a></li>
+      <li class="right"><a href="#menu">Menu</a></li>
+      <li><a className="logo" href="#top"><img src="./images/logo.jpg" title="Logo" /></a></li>
+    </ul>
+
   )
 }
