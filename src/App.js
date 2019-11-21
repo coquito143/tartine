96e0002e622f4ada4cdf448ba44603c31bdbb509
Melanie Phan <melanie.phan@prudential.com>
2019-11-21 09:35:15 -0500

    converted App.js to class component and set state variables

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './App.css';
 import {Route} from 'react-router-dom'
 import Nav from './components/Nav'
@@ -7,19 +7,50 @@ import Menu from './components/Menu'
 import HomePageBottom from './components/HomePageBottom'
 import Gallery from './components/Gallery'
 
-function App() {
-  return (
-    <div className="App">
-      <Nav />
+class App extends Component {
+  state = { 
+    menu_selected: false,
+    gallery_selected: false
+  }
 
-      <Route exact path="/" render={() => <HomeSlider />}></Route>
-      <Route path="/gallery" render={() => <Gallery />}></Route>
-      <Route path="/menu" render={() =>
-            <Menu />}
-          />
-      <HomePageBottom />
-    </div>
-  );
+  handleClick = async (selection) => {
+    if (selection == 'menu') {
+      debugger
+      this.setState({
+        menu_selected: true,
+        gallery_selected: false
+      })
+    }
+    else if (selection == 'gallery') {
+      debugger
+      this.setState({
+        menu_selected: false,
+        gallery_selected: true
+      })
+    }
+    else if (selection == 'home') {
+      debugger
+      this.setState({
+        menu_selected: false,
+        gallery_selected: false
+      })
+    }
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <Nav />
+
+        <Route exact path="/" render={() => <HomeSlider />}></Route>
+        <Route path="/gallery" render={() => <Gallery />}></Route>
+        <Route path="/menu" render={() =>
+          <Menu />}
+        />
+        <HomePageBottom />
+      </div>
+    );
+  }
 }
 
 export default App;
