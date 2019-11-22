f084690e18b942ef0ade0dc868e6ca8321a9d564
Melanie Phan <melanie.phan@prudential.com>
2019-11-21 19:45:44 -0500

    Updates

--- README.old.md
+++
@@ -1,3 +0,0 @@
-# tartine
-
-testing 123
---
+++ precache-manifest.b353d1b1176e4f165c2ce8778dce0063.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6e8f59f32919eacc82a2adb46efee1d9",
+    "url": "/repository-name/index.html"
+  },
+  {
+    "revision": "f8e02968f12cb0c0e117",
+    "url": "/repository-name/static/css/2.9d04c5f8.chunk.css"
+  },
+  {
+    "revision": "630c5c6f001553c9db6b",
+    "url": "/repository-name/static/css/main.414979a6.chunk.css"
+  },
+  {
+    "revision": "f8e02968f12cb0c0e117",
+    "url": "/repository-name/static/js/2.581ec7a1.chunk.js"
+  },
+  {
+    "revision": "630c5c6f001553c9db6b",
+    "url": "/repository-name/static/js/main.7d11a25d.chunk.js"
+  },
+  {
+    "revision": "d5396df2ea67d51337b6",
+    "url": "/repository-name/static/js/runtime-main.dee4319b.js"
+  },
+  {
+    "revision": "58b3286838ee817d7981d71221226b14",
+    "url": "/repository-name/static/media/FrutigerLTStd-Bold.58b32868.otf"
+  },
+  {
+    "revision": "57f6477d3f878af87307876a22d126af",
+    "url": "/repository-name/static/media/FrutigerLTStd-Light.57f6477d.otf"
+  },
+  {
+    "revision": "1edd1a4dd9b2a441b39012ae04d93149",
+    "url": "/repository-name/static/media/FrutigerLTStd-Roman.1edd1a4d.otf"
+  }
+]);
